1dcdc9b978ab919ea5a4e4ec8d18a49340456405
Aneta-test <Aneta.Odnova@gmailcom>
2021-04-07 15:18:40 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -3,6 +3,6 @@
 module "network" {
   source  = "app.terraform.io/ELA2-training/network/azurerm"
   version = "3.0.1"
-  resource_group_name = "Guravsvnet"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
 
